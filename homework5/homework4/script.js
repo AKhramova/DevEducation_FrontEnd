e762e12dd8875f06954025bf644ca2813b0e64bd
Anastasiia <khramovanastasiia@gmail.com>
2021-03-31 20:20:56 +0300

    error correction

--- homework5/homework4/script.js
+++ homework5/homework4/script.js
@@ -8,32 +8,36 @@ var generate = false;
 var arrayValue = [], arrayAnswer = [];
 var randomNumber, minValue, maxValue;
 
-//сheck float and minus
-function checkFloatInput(min, max){
-    if(min.value.indexOf(".") != '-1' || min.value.indexOf('-') != '-1'){
-        min.value = min.value.substr(0, min.value.indexOf('.') +1, min.value.indexOf('-') +1);
+function isValid(minValueCheck, maxValueCheck){
+    if(minValueCheck <= 0 || maxValueCheck <=0 ){
+        return false
     }
-    if(max.value.indexOf(".") !== '-1' || max.value,indexOf('-') != "-1"){
-        max.value = max.value.substr(0, max.value.indexOf('.') +1, max.value.indexOf('-') +1);
+    if(minValueCheck % 1 !== 0 || maxValueCheck % 1 !==0 ){
+        return false
     }
+    if( minValueCheck >= maxValueCheck){
+        return false
+    }
+}
+
+function randomNumbers(minValue, maxValue){
+    return Math.floor(Math.random()*(maxValue - minValue) + minValue);
 }
 
 function generateNumber(){
     minValue = Number(min.value);  
     maxValue = Number(max.value);
-    if(!generate){
-        if(maxValue !== minValue){
-            if(maxValue > 0 && minValue > 0){
-                if(maxValue > minValue){
-                    textFinally.textContent = "Start Game";
-                    for(var i = 0; i <= maxValue - minValue; i++){
-                        arrayValue[i] = minValue + i;
-                    }
-                    generate = true;
-                } else textFinally.textContent = "Incorrect input data: max < min";
-            } else textFinally.textContent = "Incorrect input data: max>0 or min>0";
-        } else textFinally.textContent = "Incorrect input data: min = max";
-        return
+
+    if(isValid(minValue, maxValue) !== false){
+        textFinally.textContent = "Start Game";
+        if(!generate){
+            for(var i = 0; i <= maxValue - minValue; i++){
+                arrayValue[i] = minValue + i;
+            }
+            generate = true;
+        }
+    } else {
+        return  textFinally.textContent = "Invalid data";
     }
     if(arrayValue.length === 0){
         buttonGenerate.disabled = true;
@@ -45,13 +49,10 @@ function generateNumber(){
 
         textFinally.textContent  = str + arrayAnswer ;
         arrayValue.splice(randomNumber, 1);
-}     
+} 
 
-function randomNumbers(minValue, maxValue){
-    return Math.floor(Math.random()*(maxValue - minValue) + minValue);
-}
 function refreshPage(){
     window.location.reload();
 }
+
 buttonGenerate.addEventListener('click', generateNumber);
-buttonReset.addEventListener('click', resetData);
