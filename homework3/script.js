429b0da754b2e02ed5571cd42d595b276e2656b9
Anastasiia <khramovanastasiia@gmail.com>
2021-03-31 19:10:12 +0300

    change homework 3

--- homework3/script.js
+++ homework3/script.js
@@ -1,101 +1,106 @@
-var countClickGenerate = 0, min = 1, max=100, attemps=5, attempsLeft, countClickChange=1;
+var count = 0, min = 1, max=100, attemps=5, countClickChange=1;
+var isGenerate = true;
 
 //block main
-var buttonGenerate = document.querySelector('.generate');
-var textStart = document.querySelector('.text-start');
-var numberAnswer = document.querySelector('.answer');
-var hints = document.querySelector('.hints');
-var textFinally = document.querySelector('.text-finally')
+var buttonGenerate = document.querySelector('.data__generate');
+var buttonExit = document.querySelector('.data__exit');
+var textStart = document.querySelector('.data__text-start');
+var numberAnswer = document.querySelector('.data__answer');
+var hints = document.querySelector('.data__hints');
+var textFinally = document.querySelector('.data__text-finally')
 
 //block change setting
-var attempsChange = document.querySelector('.attemps');
-var minChange = document.querySelector('.minValue');
-var maxChange = document.querySelector('.maxValue');
-var changeButton = document.querySelector('.change');
+var attempsChange = document.querySelector('.setting__attemps');
+var minChange = document.querySelector('.setting__min-value');
+var maxChange = document.querySelector('.setting__max-value');
+var changeButton = document.querySelector('.setting__change');
 
-//проверка на плавающую точку
-function checkFloat(numberAnswer, attempsChange, minChange, maxChange){
-    if (numberAnswer.value.indexOf(".") != '-1') {
-        numberAnswer.value=numberAnswer.value.substr(0, numberAnswer.value.indexOf(".") + 1);
-    } else if (attempsChange.value.indexOf(".") != '-1') {
-        attempsChange.value=attempsChange.value.substr(0, attempsChange.value.indexOf(".") + 1);
-    } else if (minChange.value.indexOf(".") != '-1') {
-        minChange.value=minChange.value.substr(0, minChange.value.indexOf(".") + 1);
-    } else if (maxChange.value.indexOf(".") != '-1') {
-        maxChange.value=maxChange.value.substr(0, maxChange.value.indexOf(".") + 1);
+// is valid min, max, attemps (input)
+function isValid(attempsValue, minValue, maxValue){
+    if(attempsValue <= 0 || minValue <= 0 || maxValue <=0){
+        return false
     }
+    if(attempsValue % 1 !== 0 || minValue % 1 !== 0 || maxValue % 1 !== 0){
+        return false
+    }
+    if(attempsValue > 15){
+        return false
+    }
+    if(minValue >= maxValue){
+        return false
+    }
+    if(maxValue > 200){
+        return false
+    }
+
 }
-checkFloat(numberAnswer, attempsChange, minChange, maxChange);
 
 //block change setting
 function changeSetting(){
     var userAttemps = Number(attempsChange.value);
     var userMin = Number(minChange.value);
     var userMax = Number(maxChange.value);
-    var wrong ="";
-    if(countClickChange === 1 ){
-        textStart.textContent="";
-        if(userAttemps >= 1 && userAttemps <=15){
-            attemps = userAttemps; 
-        } else{
-            wrong +="Диапазон попыток от 1 до 15. ";
-        } 
-        if(userMin > 0 ){
-            if(userMax !== userMin){
-                if(userMax > userMin && userMax <=200){
-                    min = userMin;
-                    max = userMax;
-                } else wrong +="Диапазон max числа от min до 200. ";
-            } else wrong ="max = min. ";
-        } else wrong +="min число должно быть больше 0. ";
-      textStart.textContent = wrong;   
+    textFinally.textContent = "";
+    if(isValid(userAttemps, userMin, userMax) !== false){
+        min = userMin;
+        max = userMax;
+        attemps = userAttemps;
+    } else {
+        textFinally.textContent = "Вы не правильно ввели значение j";
+    }   
+}
+
+// is valid answer
+function isValidAnswer(answerValue){ 
+    if(answerValue < min || answerValue > max){
+        return false
     }
-    randomNumber = Math.ceil(Math.random() * max); //параметры заданые пользователем
+    if(answerValue % 1 !== 0){
+        return false
+    }
 }
 
-    randomNumber = Math.ceil(Math.random() * max); //если параметры по умолчанию
 
-//block main
 function check(){
-    
     var userAnswer = Number(numberAnswer.value);
-        if(userAnswer >= min && userAnswer <= max){
-            if(countClickGenerate >= 0){
-                textStart.textContent = "Привет, я загадал число от " + min + " до " + max +
-                " вашего диапазона. Попробуй угадать его за " + attemps + " попыток!";
-                if (userAnswer >= 1){
-                    if(userAnswer === randomNumber){
-                        textFinally.textContent = "Поздравляю! Ты угадал задуманное число за " + countClickGenerate + " попыток";
-                        disabledButton();
-                    } else if (countClickGenerate === attemps){
-                        textFinally.textContent = "Ты проиграл, попробуй заново";
-                        attempsLeft=0;
-                    } else{
-                        if(attemps-countClickGenerate > 0){
-                            attempsLeft=attemps-countClickGenerate-1;
-                        } else attempsLeft=0;
-                        if(userAnswer < randomNumber){
-                            textStart = ' ';
-                            hints.textContent = "Число маленькое, введи больше. У тебя осталось: " + attempsLeft  +" попыток";
-                        } else if(userAnswer > randomNumber){
-                            hints.textContent = "Большое, введи меньше число. У тебя осталось: " + attempsLeft +" попыток";
-                        }
-                    }
-                }
+    changeSetting();
+
+    if(isGenerate === true){
+        randomNumber = Math.ceil(Math.random() * max); 
+    } 
+    count++;
+
+    if(isValidAnswer(userAnswer) !== false){
+        textStart.textContent = "Привет, я загадал число от " + min + " до " + max +
+        " вашего диапазона. Попробуй угадать его за " + attemps + " попыток!";
+        attempsChange.disabled = true;
+        minChange.disabled = true;
+        maxChange.disabled = true;
+        changeButton.disabled = true;
+        isGenerate = false;
+        if(count !== attemps){
+            if(userAnswer === randomNumber){
+                textFinally.textContent = "Поздравляю! Ты угадал задуманное число за " + count + " попыток";
+                buttonGenerate.disabled = true;
+            } else if (userAnswer > randomNumber){
+                hints.textContent = "Большое, введи меньше число. У тебя осталось: " + (attemps - count) +  " попыток" ;
+            } else {
+                hints.textContent = "Число маленькое, введи больше. У тебя осталось: " + (attemps - count) +" попыток";
             }
-        } else {  textStart.textContent ="Вы не правильно ввели число";
-                  hints.textContent='';
-            }
-        countClickGenerate++;
+        } else{
+            textFinally.textContent = "Ты проиграл, попробуй заново";
+            hints.textContent = "";
+            buttonGenerate.disabled = true; 
+        } 
+    } else{
+        count--;
+        textStart.textContent = "Вы не правильно ввели значение m";  
+    } 
 }
-function disabledButton(){
-    buttonGenerate.disabled = true;
-}
-//block exit
+    
 function refreshPage(){
     window.location.reload();
 }
 
-buttonGenerate.addEventListener('click', check);
-buttonExit.addEventListener('click', exitButton);
 changeButton.addEventListener('click', changeSetting);
+buttonGenerate.addEventListener('click', check);
---
+++ homework6/script.js
@@ -0,0 +1,16 @@
+/*Составьте алгоритм, который считает, сколько времени вам нужно на приготовление яиц.
+Правила:
+-Яйца варить 5 минут
+-Вместительность емкости не более 5 яиц одновременно
+
+function getCookingTime (eggsAmount) {
+let result;
+//Your code here
+
+return result;
+}
+
+getCookingTime(0); //returns 0
+getCookingTime(5); //returns 5
+getCookingTime(9); //returns 10 (because capacity is 5 so we need to do it 2 times)
+*/
